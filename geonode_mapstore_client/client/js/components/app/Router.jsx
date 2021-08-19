26b86b4272f171fa62b2219bd238219987ff0f72
allyoucanmap <stefano.bovio@geosolutionsgroup.com>
2021-08-19 11:44:02 +0200

    remove components and configuration not related to branch 3.3.x

--- geonode_mapstore_client/client/js/components/app/Router.jsx
+++ geonode_mapstore_client/client/js/components/app/Router.jsx
@@ -16,7 +16,6 @@ import Theme from '@mapstore/framework/components/theme/Theme';
 import { ErrorBoundary } from 'react-error-boundary';
 import history from '@mapstore/framework/stores/History';
 import ErrorFallback from '@js/components/app/ErrorFallback';
-import RootStyle from '@js/components/theme/RootStyle';
 
 export const withRoutes = (routes) => (Component) => {
     const WithRoutes = forwardRef((props, ref) => {
@@ -56,14 +55,10 @@ const Router = forwardRef(({
     pluginsConfig,
     themeCfg,
     loaderComponent,
-    geoNodeConfiguration,
     lazyPlugins
 }, ref) => {
     return (
         <>
-            <RootStyle
-                theme={geoNodeConfiguration.theme}
-            />
             <ThemeLoader
                 themeCfg={themeCfg}
                 loaderComponent={loaderComponent}
@@ -99,13 +94,6 @@ const Router = forwardRef(({
                                                     plugins={plugins}
                                                     pluginsConfig={pluginsConfig}
                                                     loaderComponent={loaderComponent}
-
-                                                    theme={geoNodeConfiguration.theme}
-                                                    navbar={geoNodeConfiguration.navbar}
-                                                    menu={geoNodeConfiguration.menu}
-                                                    footer={geoNodeConfiguration.footer}
-                                                    filters={geoNodeConfiguration.filters}
-
                                                     {...routeConfig}
                                                 />}
                                         />
