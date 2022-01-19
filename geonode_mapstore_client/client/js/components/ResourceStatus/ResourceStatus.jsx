73b57f891098d9402e3eeed2380942e07fba062c
DavidQuartz <quartzdavid@gmail.com>
2022-01-19 16:45:08 +0000

    Changed unplished labels and styles

--- geonode_mapstore_client/client/js/components/ResourceStatus/ResourceStatus.jsx
+++ geonode_mapstore_client/client/js/components/ResourceStatus/ResourceStatus.jsx
@@ -22,21 +22,21 @@ const ResourceStatus = ({ resource = {} }) => {
     } = getResourceStatuses(resource);
     return !isEmpty(resource)
         ? (
-            <p>
-                {(!isProcessing && !isApproved) && <span className={'gn-resource-status gn-resource-status-warning'} >
-                    <Message msgId={'gnviewer.underApproval'} />
+            <p className="gn-resource-status-text">
+                {(!isProcessing && !isApproved) && <span className="gn-resource-status gn-resource-status-pending" >
+                    <Message msgId="gnviewer.notApproved" />
                 </span>}
-                {(!isProcessing && !isPublished) && <span className={'gn-resource-status gn-resource-status-danger'} >
-                    <Message msgId={'gnviewer.unpublish'} />
+                {(!isProcessing && !isPublished) && <span className="gn-resource-status gn-resource-status-pending" >
+                    <Message msgId="gnhome.unpublished" />
                 </span>}
-                {isDeleting && <span className={'gn-resource-status gn-resource-status-danger'} >
-                    <Message msgId={'gnviewer.deleting'} />
+                {isDeleting && <span className="gn-resource-status gn-resource-status-danger" >
+                    <Message msgId="gnviewer.deleting" />
                 </span>}
-                {isDeleted && <span className={'gn-resource-status gn-resource-status-danger'} >
-                    <Message msgId={'gnviewer.deleted'} />
+                {isDeleted && <span className="gn-resource-status gn-resource-status-danger" >
+                    <Message msgId="gnviewer.deleted" />
                 </span>}
-                {isCopying && <span className={'gn-resource-status gn-resource-status-primary'} >
-                    <Message msgId={'gnviewer.cloning'} />
+                {isCopying && <span className="gn-resource-status gn-resource-status-primary" >
+                    <Message msgId="gnviewer.cloning" />
                 </span>}
             </p>
         )
