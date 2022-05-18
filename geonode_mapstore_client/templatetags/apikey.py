3b7df8bdac00d98d630c1338fb2a93991743e33d
mattiagiupponi <mattia.giupponi@gmail.com>
2022-05-18 17:50:17 +0200

    Fix PR changes

--- geonode_mapstore_client/templatetags/apikey.py
+++ geonode_mapstore_client/templatetags/apikey.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright 2015-2022, GeoSolutions Sas.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
+
 import logging
 from urllib.parse import urlparse
 
