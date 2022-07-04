87357f3daf11c933fee82de1d73c418fcd7db579
DavidQuartz <quartzdavid@gmail.com>
2022-07-04 12:05:10 +0000

    Externalized supported files types

--- geonode_mapstore_client/context_processors.py
+++ geonode_mapstore_client/context_processors.py
@@ -54,6 +54,57 @@ def resource_urls(request):
                     'OPTIONS',
                     dict()).get(
                         'MOSAIC_ENABLED',
-                        False)
+                        False),
+        'SUPPORTED_DATASET_FILE_TYPES': getattr(settings, "MAPSTORE_SUPPORTED_DATASET_FILE_TYPES", [
+                {
+                    "id": "shp",
+                    "label": "ESRI Shapefile",
+                    "format": "vector",
+                    "ext": ["shp"],
+                    "requires": ["shp", "prj", "dbf", "shx"],
+                    "optional": ["xml", "sld"]
+                },
+                {
+                    "id": "tiff",
+                    "label": "GeoTIFF",
+                    "format": "raster",
+                    "ext": ["tiff", "tif"],
+                    "mimeType": ["image/tiff"],
+                    "optional": ["xml", "sld"]
+                },
+                {
+                    "id": "csv",
+                    "label": "Comma Separated Value (CSV)",
+                    "format": "vector",
+                    "ext": ["csv"],
+                    "mimeType": ["text/csv"],
+                    "optional": ["xml", "sld"]
+                },
+                {
+                    "id": "zip",
+                    "label": "Zip Archive",
+                    "format": "archive",
+                    "ext": ["zip"],
+                    "mimeType": ["application/zip"],
+                    "optional": ["xml", "sld"]
+                },
+                {
+                    "id": "xml",
+                    "label": "XML Metadata File",
+                    "format": "metadata",
+                    "ext": ["xml"],
+                    "mimeType": ["application/json"],
+                    "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "sld"]
+                },
+                {
+                    "id": "sld",
+                    "label": "Styled Layer Descriptor (SLD)",
+                    "format": "metadata",
+                    "ext": ["sld"],
+                    "mimeType": ["application/json"],
+                    "needsFiles": ["shp", "prj", "dbf", "shx", "csv", "tiff", "zip", "xml"]
+                }
+            ]
+        )
     }
     return defaults
