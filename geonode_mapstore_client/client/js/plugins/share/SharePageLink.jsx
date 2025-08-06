b16db2030b1813ff4a74d97c7ff86940efb73862
Suren <dsuren1@gmail.com>
2025-08-06 11:11:39 +0530

    Move permissions management back to the Share panel

--- geonode_mapstore_client/client/js/plugins/share/SharePageLink.jsx
+++ geonode_mapstore_client/client/js/plugins/share/SharePageLink.jsx
@@ -11,10 +11,13 @@ import { Glyphicon } from 'react-bootstrap';
 
 import Button from '@mapstore/framework/components/layout/Button';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import FlexBox from '@mapstore/framework/components/layout/FlexBox';
 
 
-function SharePageLink({label, value, children}) {
+function SharePageLink({label, value, collapsible = true, children}) {
     const [copied, setCopied] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
+
     useEffect(() => {
         if (copied) {
             setTimeout(() => {
@@ -22,30 +25,48 @@ function SharePageLink({label, value, children}) {
             }, 1000);
         }
     }, [copied]);
+
     return (
         <div className="gn-share-link-pad">
             <div className="gn-share-link-wrapper">
                 <div className="gn-share-page-link">
-                    <label className="gn-share-title">{label}</label>
-                    <div className="gn-share-link">
-                        <input
-                            readOnly
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            value={value}
-                        />
-                        {!copied && <CopyToClipboard
-                            text={value}
+                    {label && (
+                        <FlexBox
+                            centerChildrenVertically
+                            className="gn-share-title-header"
+                            {...(collapsible && {
+                                onClick: () => setIsExpanded(!isExpanded),
+                                style: { cursor: 'pointer' }
+                            })}
                         >
-                            <Button
-                                size="sm"
-                                onClick={() => setCopied(true)}
-                            >
-                                <Glyphicon glyph="copy" />
-                            </Button>
-                        </CopyToClipboard>}
-                        {copied && <Button size="sm"><Glyphicon glyph="ok" /></Button>}</div>
-                    {children}
+                            <label className="gn-share-title">{label}</label>
+                            {collapsible && <Glyphicon glyph={isExpanded ? "chevron-down" : "chevron-right"}/>}
+                        </FlexBox>
+                    )}
+                    {(!collapsible || isExpanded) && (
+                        <div className="gn-share-content">
+                            <div className="gn-share-link">
+                                <input
+                                    readOnly
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    value={value}
+                                />
+                                {!copied && <CopyToClipboard
+                                    text={value}
+                                >
+                                    <Button
+                                        size="sm"
+                                        onClick={() => setCopied(true)}
+                                    >
+                                        <Glyphicon glyph="copy" />
+                                    </Button>
+                                </CopyToClipboard>}
+                                {copied && <Button size="sm"><Glyphicon glyph="ok" /></Button>}
+                            </div>
+                            {children}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
