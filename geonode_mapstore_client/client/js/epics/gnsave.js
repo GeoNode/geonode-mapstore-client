54c993e308fefb3edb52b11da971f733fc6ba0c6
Suren <dsuren1@gmail.com>
2025-11-01 17:12:15 +0530

    [Fixes #2184] Saving map layer settings for a dataset calls the wrong URL (#2185)

--- geonode_mapstore_client/client/js/epics/gnsave.js
+++ geonode_mapstore_client/client/js/epics/gnsave.js
@@ -65,7 +65,8 @@ import {
     getResourceId,
     getDataPayload,
     getCompactPermissions,
-    getExtentPayload
+    getExtentPayload,
+    getInitialDatasetLayerStyle
 } from '@js/selectors/resource';
 
 import {
@@ -113,9 +114,9 @@ const setDefaultStyle = (state, id) => {
         availableStyles =  [...defaultStyle, ...filteredStyles];
     }
     const {style: currentStyleName} = getSelectedNode(state) ?? {};
-    const initalStyleName = layer?.availableStyles?.[0]?.name;
+    const initialStyleName = getInitialDatasetLayerStyle(state);
 
-    if (id && initalStyleName && currentStyleName !== initalStyleName) {
+    if (id && initialStyleName && currentStyleName !== initialStyleName) {
         const { baseUrl = '' } = styleServiceSelector(state);
         return {
             request: () => LayersAPI.updateDefaultStyle({
@@ -228,7 +229,7 @@ export const gnSaveContent = (action$, store) =>
             const { compactPermissions } = getPermissionsPayload(state);
             return Observable.defer(() => SaveAPI[contentType](state, action.id, body, action.reload))
                 .switchMap((response) => {
-                    const [resource, ...actions] = castArray(response);
+                    let [resource, ...actions] = castArray(response);
                     if (action.reload) {
                         if (contentType === ResourceTypes.VIEWER) {
                             const sourcepk = get(state, 'router.location.pathname', '').split('/').pop();
@@ -238,6 +239,11 @@ export const gnSaveContent = (action$, store) =>
                         window.location.reload();
                         return Observable.empty();
                     }
+                    const selectedLayer = getSelectedNode(state);
+                    const currentStyle = selectedLayer?.availableStyles?.find(({ name }) => selectedLayer?.style?.includes(name));
+                    // adding default style upon saving resource for correct style comparison
+                    const defaultStyle =  currentStyle ? { sld_title: currentStyle.title, name: selectedLayer?.style } : null;
+                    resource = {...resource, default_style: defaultStyle};
                     return Observable.merge(
                         Observable.of(
                             saveSuccess(resource),
